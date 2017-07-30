57bff13638d769757d8c8ba4922a4d5615e9a07e
patiuma666 <patiuma666@gmail.com>
2017-07-30 23:12:14 +0530

    Add files via upload

---
+++ ThrowDemo.java
@@ -0,0 +1,27 @@
+
+package assignment12;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ThrowDemo {
+	 static int age;
+	 public static void main(String args[])throws Exception{  
+	       
+		 System.out.print("enter age");
+		 Scanner sc =new Scanner(System.in);
+		    int Scanner=sc.nextInt();
+		    
+	     if(age<18) { 
+	    	
+	      System.out.println("not eligible for vote"); 
+	     }
+	    	 else {
+	    	 
+	    {
+	      System.out.println("eligible for vote");  
+	    	 
+	}}
+	    	 }
+	    	    
+	      }
